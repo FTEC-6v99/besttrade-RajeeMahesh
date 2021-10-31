5c72d321929f35a6faed6d9b091743cdc4b983c9
Raji <RXU200002@utdallas.edu>
2021-10-31 03:31:11 -0500

    Assignment-2

--- app/src/main.py
+++ app/src/main.py
@@ -3,8 +3,15 @@ import app.src.db.dao as dao
 def main():
     inv = dao.get_all_investor()
     for i in inv:
-        print(i.id)
+        print(f'Id: {i.id} - NAME----name: {i.name} - status: {i.status}')
 
+    
+    acc = dao.get_all_accounts()
+    for a in acc:
+        print(f'account no: {a.account_number} - invester id: {a.investor_id} - balance: {a.balance} ')
+
+    dao.buy_stock(7, 'T', 5, 9.1)
+    dao.sell_stock(3, 'RECO', 5, 7.00)
 
 
 if __name__ == '__main__':
